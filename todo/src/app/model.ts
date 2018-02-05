083685a36b101869eeca3c3eaf438806bdff1bc1
altair961 <altair961@github.com>
2018-02-05 17:50:53 +0400

    Redefined model.ts using TypeScript

--- todo/src/app/model.ts
+++ todo/src/app/model.ts
@@ -1,7 +1,21 @@
-var model = {
-  user: "Adam",
-  items: [{ action: "Buy Flowers", done: false },
-  { action: "Get Shoes", done: false },
-  { action: "Collect Tickets", done: true },
-  { action: "Call Joe", done: false}]    
+export class Model {
+  user;
+  items;
+  constructor() {
+    this.user = "Adam"
+    this.items = [new TodoItem("Buy Flowers", false),
+                  new TodoItem("Get Shoes", false),
+                  new TodoItem("Collect Tickets", true),
+                  new TodoItem("Call Joe", false)]
+  }
+}
+
+export class TodoItem {
+  acion;
+  done;
+
+  constructor(action, done){
+    this.action = action;
+    this.done = done;
+  }
 }
