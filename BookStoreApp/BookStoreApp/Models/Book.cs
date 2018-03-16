447c4fa73180d807b4eed40a43900152a5e06c81
altair961 <altair961@github.com>
2018-03-16 05:16:28 +0400

    Added book model and purchase model

---
+++ BookStoreApp/BookStoreApp/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookStoreApp.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ BookStoreApp/BookStoreApp/Models/Purchase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BookStoreApp.Models
+{
+    public class Purchase
+    {
+        public Guid PurchaseId { get; set; }
+        public string Person { get; set; }
+        public string Address { get; set; }
+        public Guid BookId { get; set; }
+        public DateTime date { get; set; }
+    }
+}
