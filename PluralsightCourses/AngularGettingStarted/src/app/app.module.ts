c2545169ff3f95cb9fad9eb749643e41af0591d9
altair961 <altair89@list.ru>
2020-01-05 18:58:59 +0300

    Added nested component and exposed its field

--- PluralsightCourses/AngularGettingStarted/src/app/app.module.ts
+++ PluralsightCourses/AngularGettingStarted/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
-import { from } from 'rxjs';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ConvertToSpacesPipe
+    ConvertToSpacesPipe,
+    StarComponent
   ],
   imports: [
     BrowserModule,
