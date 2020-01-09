671aea0fcd50e8b47eed1267ab3bf32abadcacbf
altair961 <altair89@list.ru>
2020-01-09 08:01:26 +0300

    Enabled routing initiated by code

--- PluralsightCourses/AngularGettingStarted/src/app/products/product-detail.component.ts
+++ PluralsightCourses/AngularGettingStarted/src/app/products/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,7 +10,8 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     let id = +this.route.snapshot.paramMap.get('id'); // + sign is a js shortcut to convert string parameter to a numeric id
@@ -24,6 +25,10 @@ export class ProductDetailComponent implements OnInit {
       "price": 19.95,
       "starRating": 3.2,
       "imageUrl": "assets/images/leaf_rake.png"
-    }
+    };
+  }
+  
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 }
